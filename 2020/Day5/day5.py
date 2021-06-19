d6b57d6643ca13d33f7e8c5ae14e660c0c137e38
ricardo-lopes3 <ricardo.lopes3@vodafone.com>
2021-06-19 15:33:18 +0100

    day5 done

--- 2020/Day5/day5.py
+++ 2020/Day5/day5.py
@@ -16,20 +16,33 @@ raw = r'2020\Day5\test.txt'
 #########
 
 
+def calc_seat_id(row, column,multiple):
+    return row * multiple + column
+
+def read_binary_cols(my_string):
+    row = int(my_string[:7].replace('F','0').replace('B','1'),2)
+    column = int(my_string[7:].replace('L','0').replace('R','1'),2)
+    id = calc_seat_id(row, column,8)
+    return (row, column, id)
+
 def main(raw,part):
     # read inputs from file
     input = tools.read_input(raw)    
     if part == 1:        
-        pass
+        bps_result = [read_binary_cols(bp) for bp in input]
+        return max([bp[2] for bp in bps_result])
     elif part == 2:        
-        pass
+        bps_result = [read_binary_cols(bp) for bp in input]
+        bps_ids = [bp[2] for bp in bps_result]
+        return [b+1 for a in bps_ids for b in bps_ids if (b+2==a and b+1 not in bps_ids)]
+        
     else:
         raise ValueError("part must be 1 or 2, instead of: " + part)
     
 
 def run_tests():    
-    assert main(test_raw,1) == 2    
-    assert main(test_raw,2) == 0
+    assert main(test_raw,1) == 820  
+    #assert main(test_raw,2) == 0
 
 
 if __name__ == '__main__':
