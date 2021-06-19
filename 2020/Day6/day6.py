b068af82bb70f6f941ef75be5799c3edf106e5d6
ricardo-lopes3 <ricardo.lopes3@vodafone.com>
2021-06-19 19:34:03 +0100

    day6 done

--- 2020/Day6/day6.py
+++ 2020/Day6/day6.py
@@ -14,28 +14,41 @@ raw = r'2020\Day6\test.txt'
 # Start #
 #########
 
-def count_questions():
-    pass
+def count_any_yes(group):
+    return len(set(''.join(group)))
+
+def count_everyone_yes(group):
+    people = len(group)
+    all_chars = ''.join(group)
+    distinct_chars = list(set(''.join(group)))
+    return len([x for x in distinct_chars if all_chars.count(x) == people])
+
 
 def main(raw,part):
     # read inputs from file
     input = tools.read_input_blank_separator(raw)    
     if part == 1:                
-        pass
+        return sum([count_any_yes(group) for group in input])
     elif part == 2:        
-        pass        
+        return sum([count_everyone_yes(group) for group in input])
     else:
         raise ValueError("part must be 1 or 2, instead of: " + part)
     
 
 def run_tests():    
     assert main(test_raw,1) == 11
-    #assert main(test_raw,2) == 0
+    test_input = tools.read_input_blank_separator(raw)    
+    assert count_everyone_yes(test_input[0]) == 3
+    assert count_everyone_yes(test_input[1]) == 0
+    assert count_everyone_yes(test_input[2]) == 1
+    assert count_everyone_yes(test_input[3]) == 1
+    assert count_everyone_yes(test_input[4]) == 1
+    assert main(test_raw,2) == 6
 
 
 if __name__ == '__main__':
     run_tests()
     answer1 = main(input_raw,1)    
-    #answer2 = main(input_raw,2)    
+    answer2 = main(input_raw,2)    
     print("Answer part1: {}".format(answer1))        
-    #print("Answer part2: {}".format(answer2))    
+    print("Answer part2: {}".format(answer2))    
