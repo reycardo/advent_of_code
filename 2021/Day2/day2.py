0d5396abef0a4e18f3d4d941af9026ebe4e1d7fc
ricardo-lopes3 <ricardo.lopes3@vodafone.com>
2021-12-02 12:29:25 +0000

    day2 done

--- 2021/Day2/day2.py
+++ 2021/Day2/day2.py
@@ -11,43 +11,57 @@ test_raw = os.path.join(__location__,'test.txt')
 # Start #
 #########
 
-def count(input):
-    acc = 0
-    for i in range(len(input)-1) :
-        if input[i+1] > input[i]:
-            acc += 1 
-    return acc
+def parse_input(input):            
+    input = [x.split(' ') for x in input if x]
+    return [(i[0],int(i[1])) for i in input]
 
-def count_slide(input):
-    acc = 0
-    for i in range(len(input)-2) :
-        if i == 0:
-            pass
-        elif input[i] + input[i+1] + input[i+2] > input[i-1] + input[i] + input[i+1]:
-            acc += 1 
-    return acc
+def get_pos(input):
+    depth = 0
+    h = 0
+    for i in range(len(input)):
+        if input[i][0] == 'forward':
+            h += input[i][1]
+        elif input[i][0] == 'up':
+            depth -= input[i][1]
+        elif input[i][0] == 'down':
+            depth += input[i][1]                        
+    return depth, h
+
+def get_pos_aim(input):
+    depth = 0
+    h = 0
+    aim = 0
+    for i in range(len(input)):
+        if input[i][0] == 'forward':
+            h += input[i][1]
+            depth += aim * input[i][1]
+        elif input[i][0] == 'up':
+            aim -= input[i][1]
+        elif input[i][0] == 'down':
+            aim += input[i][1]                        
+    return depth, h
 
 def main(raw,part):
     # read inputs from file
     input = tools.read_input(raw)
-    
-    # convert to all elements int
-    input = [int(i) for i in input if i]
-    
+        
+    input = parse_input(input)
     if part == 1:        
-        return count(input)
+        depth, h = get_pos(input)
+        return depth * h
     elif part == 2:        
-        return count_slide(input)
+        depth, h = get_pos_aim(input)
+        return depth * h
     else:
         raise ValueError("part must be 1 or 2, instead of: " + part)
 
 
 def run_tests():
-    assert main(test_raw,1) == 7
-    assert main(test_raw,2) == 5
+    assert main(test_raw,1) == 150
+    assert main(test_raw,2) == 900
     # solutions
-    assert main(input_raw,1) == 1301
-    assert main(input_raw,2) == 1346
+    assert main(input_raw,1) == 1507611
+    assert main(input_raw,2) == 1880593125
     
 
     
