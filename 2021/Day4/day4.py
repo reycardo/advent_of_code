4d8159c2c7f2d97838cebe37b2ca98fd99af1b09
ricardo-lopes3 <ricardo.lopes3@vodafone.com>
2021-12-04 23:27:17 +0000

    d4 start

--- 2021/Day4/day4.py
+++ 2021/Day4/day4.py
@@ -14,39 +14,9 @@ test_raw = os.path.join(__location__,'test.txt')
 #########
 
 def parse_input(input):                     
+    draws = input[0].split(',')
     return pd.DataFrame([[*x] for x in input if x])
 
-def get_gamma(df):
-    return ''.join(df.mode(axis='rows').values.tolist()[0])
-
-def get_epsilon(gamma):
-    return gamma.replace('1', '2').replace('0', '1').replace('2', '0')
-
-def get_oxygen(df):
-    columns = df.columns
-    for i in columns:
-        val_counts = df[i].value_counts()
-        if len(val_counts) == 1:
-            mode = df[i].values.tolist()[0]            
-        elif val_counts['0'] > val_counts['1']:
-            mode = '0'
-        else:
-            mode = '1'
-        df = df.loc[df[i] == mode]
-    return ''.join(df.values[0])
-
-def get_CO2(df):
-    columns = df.columns
-    for i in columns:
-        val_counts = df[i].value_counts()
-        if len(val_counts) == 1:
-            mode = df[i].values.tolist()[0]            
-        elif val_counts['1'] < val_counts['0']:
-            mode = '1'
-        else:
-            mode = '0'        
-        df = df.loc[df[i] == mode]
-    return ''.join(df.values[0])
 
 def main(raw,part):
     # read inputs from file
@@ -54,23 +24,19 @@ def main(raw,part):
         
     input = parse_input(input)
     if part == 1:        
-        gamma = get_gamma(input)
-        epsilon = get_epsilon(gamma)
-        return int(gamma,2) * int(epsilon,2)
+        pass
     elif part == 2:        
-        oxygen = get_oxygen(input)
-        CO2 = get_CO2(input)
-        return int(oxygen,2) * int(CO2,2)
+        pass
     else:
         raise ValueError("part must be 1 or 2, instead of: " + part)
 
 
 def run_tests():
-    assert main(test_raw,1) == 198
-    assert main(test_raw,2) == 230
+    assert main(test_raw,1) == 4512
+    #assert main(test_raw,2) == 230
     # solutions
-    assert main(input_raw,1) == 3813416
-    assert main(input_raw,2) == 2990784
+    #assert main(input_raw,1) == 3813416
+    #assert main(input_raw,2) == 2990784
     
 
     
