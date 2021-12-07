c472ccb486011c45ce22b5990a89e8c59c4398f2
ricardo-lopes3 <ricardo.lopes3@vodafone.com>
2021-12-07 15:57:58 +0000

    done

--- 2021/Day7/day7.py
+++ 2021/Day7/day7.py
@@ -3,6 +3,8 @@ import sys
 sys.path.insert(0, './')
 from utils import tools
 from collections import Counter
+import statistics
+import math
 
 raw = r'2021\Day7\test.txt'
 
@@ -18,43 +20,38 @@ def parse_input(input):
     return [list(map(int,line.split(','))) for line in input][0]
 
 
-def new_days(input,days,max_int_timer):
-    new_babies = []
-    for day in range(days):
-        input = [fish - 1 if fish > 0 else 6 for fish in input] # new day
-        input = input + new_babies
-        new_babies = [max_int_timer] * input.count(0)
-    return len(input)
-
-def calc_fish(input,days,max_int_timer):
-    counts = Counter(input)
-    fish_counter = [0] * (max_int_timer + 1) # fish_counter[x] represents the number of fishes with internal time x
-    for i in range(max_int_timer + 1):
-        fish_counter[i] = counts[i]
-    for day in range(days):
-        new_babies = fish_counter.pop(0) # transforms 0s into 6s
-        fish_counter[6] += new_babies    # transforms 0s into 6s
-        fish_counter.append(new_babies)  # pop and append shifts everyone one position (simulates a new day)
-    return sum(fish_counter)
+def fuel(input,part):
+    if part == 1:
+        stat = int(statistics.median(input))
+        return sum([abs(crab - stat) for crab in input])
+    elif part == 2:
+        stat_plus = math.ceil(statistics.mean(input))
+        stat = round(statistics.mean(input))
+        stat_minus = math.floor(statistics.mean(input))
+        stats = [stat_minus, stat_plus, stat]
+        tries = []
+        for stat_ in stats:
+            tries.append(sum([int((abs(crab - stat_) * (abs(crab - stat_) + 1))/2) for crab in input]))
+        return min(tries)
 
 def main(raw,part):
     # read inputs from file
     input = tools.read_input(raw)
     input = parse_input(input)
     if part == 1:                                
-        return new_days(input,80,8)
+        return fuel(input,1)
     elif part == 2:        
-        return calc_fish(input,256,8)
+        return fuel(input,2)
     else:
         raise ValueError("part must be 1 or 2, instead of: " + part)
 
 
 def run_tests():
-    assert main(test_raw,1) == 5934
-    assert main(test_raw,2) == 26984457539
+    assert main(test_raw,1) == 37
+    assert main(test_raw,2) == 168
     # solutions
-    assert main(input_raw,1) == 362639
-    assert main(input_raw,2) == 1639854996917
+    assert main(input_raw,1) == 344535
+    #assert main(input_raw,2) == 1639854996917
     
 
     
