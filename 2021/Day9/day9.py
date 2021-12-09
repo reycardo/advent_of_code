ffe19c804041726408dd279b75b3695a1e8d3451
ricardo-lopes3 <ricardo.lopes3@vodafone.com>
2021-12-09 13:06:31 +0000

    done

--- 2021/Day9/day9.py
+++ 2021/Day9/day9.py
@@ -3,7 +3,9 @@ import sys
 sys.path.insert(0, './')
 from utils import tools
 import numpy as np
-
+from scipy.ndimage import measurements
+from collections import Counter
+import math
 raw = r'2021\Day9\test.txt'
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
@@ -18,33 +20,68 @@ def parse_input(input):
     return np.matrix([[int(digit) for digit in row] for row in input])
 
 def compare_adjacent(input,i,j):
-    pass
+    if j == 0:
+        l = float('inf')
+    else:
+        l = input[i,j-1]
+    if i == 0:
+        u = float('inf')
+    else:
+        u = input[i-1,j]
+    if i == input.shape[0]-1:
+        d = float('inf')
+    else:
+        d = input[i+1,j]
+    if j == input.shape[1]-1:
+        r = float('inf')
+    else:
+        r = input[i,j+1]    
+    if input[i,j] < l and input[i,j] < r and input[i,j] < u and input[i,j] < d:
+        return True
+    else:
+        return False
 
 def find_low(input):
     lows = []
-    for i in range(input.shape[0]):
-        for j in range(input.shape[1]):
+    for j in range(input.shape[1]):
+        for i in range(input.shape[0]):        
             if compare_adjacent(input,i,j):
                 lows.append(input[i,j])
     return lows
 
+def get_risk(lows):
+    return sum([low+1 for low in lows])
+
+def get_basins(input):
+    basins, total_bs = measurements.label(np.where(input < 9, True, False))
+    return basins, total_bs
+
+def find_biggest(basins,n):
+    basin_sizes = Counter(tools.flatten(basins.tolist()))
+    del basin_sizes[0]
+    return sorted(list(basin_sizes.values()))[-n:]
+
+
 def main(raw,part):
     # read inputs from file
     input = tools.read_input(raw)
-    input = parse_input(input)
-    if part == 1:                                
-        pass
+    input = parse_input(input)    
+    if part == 1:
+        lows = find_low(input)
+        return get_risk(lows)
     elif part == 2:        
-        pass
+        basins, _ = get_basins(input)
+        biggest3 = find_biggest(basins,3)
+        return math.prod(biggest3)
     else:
         raise ValueError("part must be 1 or 2, instead of: " + str(part))
 
 
 def run_tests():
     assert main(test_raw,1) == 15
-    #assert main(test_raw,2) == 61229
+    assert main(test_raw,2) == 1134
     # solutions
-    #assert main(input_raw,1) == 303
+    assert main(input_raw,1) == 572
     #assert main(input_raw,2) == 961734
     
     
