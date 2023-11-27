5e801292ed7a39473bb1c28734750978cebec8e7
reycardo <reycardo_95@hotmail.com>
2023-11-27 15:18:55 +0000

    ✨ added way to get input directly from aocd

---
+++ advent-of-code/utils/get_input.py
@@ -0,0 +1,29 @@
+import subprocess
+import sys
+import os
+
+
+def download_aoc_input(day, year):
+    # Create the output directory if it doesn't exist
+    output_dir = os.path.join(
+        os.getcwd(), "advent-of-code", "src", str(year), f"Day{day}"
+    )
+    os.makedirs(output_dir, exist_ok=True)
+
+    # Run the aocd command to download the input and redirect it to a file
+    command = f"aocd {day} {year} > {output_dir}\\input.txt"
+    subprocess.run(command, shell=True)
+
+
+if __name__ == "__main__":
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) != 3:
+        print("Usage: python my_script.py <day> <year>")
+        sys.exit(1)
+
+    # Get day and year from command-line arguments
+    day = sys.argv[1]
+    year = sys.argv[2]
+
+    # Run the function to download AOC input
+    download_aoc_input(day, year)
