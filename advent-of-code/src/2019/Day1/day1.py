5fb45121e42669df1663a588a172b78338c88457
reycardo <reycardo_95@hotmail.com>
2023-11-27 21:14:34 +0000

    ⭐️solved Day1 2019

--- advent-of-code/src/2019/Day1/day1.py
+++ advent-of-code/src/2019/Day1/day1.py
@@ -3,66 +3,62 @@ from utils.colors import magenta_color, reset_color
 from itertools import groupby
 
 files = get_txt_files(__file__)
-input_raw = files["input"]
-test_raw = files["test"]
 #########
 # Start #
 #########
 
 
-class ElfCalories:
+class Fuel:
     def __init__(self, text_input: list):
         self.input = text_input
-        self.elf_calorie_dict = self.get_elf_dict()
-        self.elf_count = len(self.elf_calorie_dict)
 
-    def get_elf_dict(self):
-        d = {
-            elf: list(sub[1])
-            for elf, sub in enumerate(groupby(self.input, key=bool))
-            if sub[0]
-        }  # groups into an enumerated dict if key is not ''
-        return {
-            k: (v, sum(v)) for k, v in d.items()
-        }  # returns dict where key is elf number, v is list of elf calories, sum(v) is total cals elf has
+    def get_module_fuel(self, mass):
+        return (mass // 3) - 2
 
-    def get_max(self, number):
-        # sorts descending, gets top number
-        return sum(
-            n
-            for _, n in sorted(
-                self.elf_calorie_dict.values(), key=lambda t: t[1], reverse=True
-            )[:number]
-        )
+    def get_module_fuel_recursive(self, mass):
+        result = (mass // 3) - 2
+        if result <= 0:
+            return 0
+        else:
+            return result + self.get_module_fuel_recursive(result)
+
+    def get_fuel(self, part):
+        if part == 1:
+            return sum([self.get_module_fuel(element) for element in self.input])
+        elif part == 2:
+            return sum(
+                [self.get_module_fuel_recursive(element) for element in self.input]
+            )
+        else:
+            raise ValueError("part must be 1 or 2, instead of: " + part)
 
 
 @timing_decorator
 def main(raw, part):
     text_input = read_input(raw)
     input_parsed = [int(i) if i else "" for i in text_input]
-    elf_cals = ElfCalories(input_parsed)
-    if part == 1:
-        return elf_cals.get_max(1)
-    elif part == 2:
-        return elf_cals.get_max(3)
-    else:
-        raise ValueError("part must be 1 or 2, instead of: " + part)
+    fuel = Fuel(input_parsed)
+    return fuel.get_fuel(part)
 
 
 def run_tests():
     print(f"\nRunning Tests:")
-    assert main(test_raw, 1) == 24000
-    assert main(test_raw, 2) == 45000
+    assert main(files["test1_1"], 1) == 654
+    assert main(files["test1_2"], 1) == 33583
+    assert main(files["test2_1"], 2) == 2
+    assert main(files["test2_2"], 2) == 966
+    assert main(files["test2_3"], 2) == 50346
+
     # solutions
-    assert main(input_raw, 1) == 66616
-    assert main(input_raw, 2) == 199172
+    assert main(files["input"], 1) == 3481005
+    assert main(files["input"], 2) == 5218616
 
 
 def run_solution():
     print(f"\nRunning Solutions:")
-    answer1 = main(input_raw, 1)
+    answer1 = main(files["input"], 1)
     print(f"Answer part1: {magenta_color}{answer1}{reset_color}")
-    answer2 = main(input_raw, 2)
+    answer2 = main(files["input"], 2)
     print(f"Answer part2: {magenta_color}{answer2}{reset_color}")
 
 
