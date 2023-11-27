069a30719d8e9250b9e5b24948330fbfeab514bf
reycardo <reycardo_95@hotmail.com>
2023-11-27 14:57:11 +0000

    🎨 Decorators

--- advent-of-code/utils/tools.py
+++ advent-of-code/utils/tools.py
@@ -1,20 +1,90 @@
 import os
+import time
+from typing import Any, Callable
+from datetime import timedelta
+from utils.colors import green_color, cyan_color, reset_color
 
-def read_input(file,sep: str = '\n'):
-    with open(file, "r") as tf:        
+
+def read_input(file, sep: str = "\n"):
+    with open(file, "r") as tf:
         return tf.read().strip().split(sep)
+
 
 def flatten(t):
     return [item for sublist in t for item in sublist]
 
+
 def get_txt_files(current_script_path):
     data_paths = {}
-    # Get the path of the current script file    
-    current_dir = os.path.dirname(current_script_path)    
+    # Get the path of the current script file
+    current_dir = os.path.dirname(current_script_path)
     # For each .txt file in the current directory and its subdirectories
     for root, _, files in os.walk(current_dir):
         for file in files:
-            if file.endswith('.txt'):
-                data_type = file.split('.')[0]
+            if file.endswith(".txt"):
+                data_type = file.split(".")[0]
                 data_paths[data_type] = os.path.join(root, file)
     return data_paths
+
+
+def timing_decorator(func: Callable[..., Any]) -> Callable[..., Any]:
+    """
+    Decorator to measure the execution time of a function.
+
+    Parameters:
+        func (callable): The function to be timed.
+
+    Returns:
+        callable: The wrapped function.
+
+    Example:
+        @timing_decorator
+        def example_function():
+            # Your function code here
+            time.sleep(2)
+            print("Function executed.")
+    """
+
+    def wrapper(*args, **kwargs) -> Any:
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        readable_time = format_elapsed_time(elapsed_time)
+        print(
+            f"{green_color}{func.__name__}{reset_color} took {cyan_color}{readable_time}{reset_color} to run."
+        )
+        return result
+
+    return wrapper
+
+
+def format_elapsed_time(seconds: float) -> str:
+    """
+    Format elapsed time in a human-readable format.
+
+    Parameters:
+        seconds (float): Elapsed time in seconds.
+
+    Returns:
+        str: Human-readable elapsed time.
+    """
+    delta = timedelta(seconds=seconds)
+
+    # Extract components
+    days = delta.days
+    hours, remainder = divmod(delta.seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    ms = delta.microseconds / 1000
+
+    # Format as a string
+    if days > 0:
+        return f"{days}d, {hours}h, {minutes}m, {seconds}s, {ms}ms"
+    elif hours > 0:
+        return f"{hours}h, {minutes}m, {seconds}s, {ms}ms"
+    elif minutes > 0:
+        return f"{minutes}m, {seconds}s, {ms}ms"
+    elif seconds > 0:
+        return f"{seconds}s, {ms}ms"
+    else:
+        return f"{ms}ms"
