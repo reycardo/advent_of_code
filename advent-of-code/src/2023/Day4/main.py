40cb85516e16c8cba3c69929ace12a26b4072e04
reycardo <reycardo_95@hotmail.com>
2023-12-06 18:40:43 +0000

    🚧 WIP D4

--- advent-of-code/src/2023/Day4/main.py
+++ advent-of-code/src/2023/Day4/main.py
@@ -1,6 +1,6 @@
 from utils.tools import get_txt_files, read_input, timing_decorator
 from utils.colors import magenta_color, reset_color
-
+import re
 
 files = get_txt_files(__file__)
 #########
@@ -11,9 +11,25 @@ files = get_txt_files(__file__)
 class Scratchcard:
     def __init__(self, text_input):
         self.input = text_input
+        self.input_parsed = [self.parse_card(card) for card in self.input ]
+        a=1
 
     def solve(self):
         pass
+
+    def parse_card(self, card_string: str):
+        sections = card_string.split(":", 1)
+        match = re.search(r"\d+", sections[0])
+
+        if match:
+            card_number = int(match.group())
+        else:
+            raise ValueError("No game number found in the input string.")
+
+        winning_numbers, my_numbers = sections[1].split("|", 1)        
+        winning_numbers = [num for num in winning_numbers.strip().split() if num]
+        my_numbers = [num for num in my_numbers.strip().split() if num]
+        return {card_number: (winning_numbers, my_numbers)}
 
 @timing_decorator
 def main(raw, part):
