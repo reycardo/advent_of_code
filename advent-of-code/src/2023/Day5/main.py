a2ef82f4ffb24d15fcf85b315e6908e20db0103f
reycardo <reycardo_95@hotmail.com>
2023-12-07 02:11:35 +0000

    🎉 Start Day5

---
+++ advent-of-code/src/2023/Day5/main.py
@@ -0,0 +1,50 @@
+from utils.tools import get_txt_files, read_input, timing_decorator
+from utils.colors import magenta_color, reset_color
+
+files = get_txt_files(__file__)
+#########
+# Start #
+#########
+
+
+class Almanac:
+    def __init__(self, text_input):
+        self.input = text_input
+
+    def solve(self, part):
+        if part == 1:
+            pass
+        if part == 2:
+            pass
+
+
+@timing_decorator
+def main(raw, part):
+    text_input = read_input(raw)
+    input_parsed = [i if i else "" for i in text_input]
+    scratchcards = Almanac(input_parsed)
+    return scratchcards.solve(part)
+
+
+def run_tests():
+    print(f"\nRunning Tests:")
+    assert main(raw=files["test"], part=1) == 13
+    assert main(raw=files["test"], part=2) == 30
+
+    # solutions
+    print(f"\nRunning Solutions:")
+    assert main(raw=files["input"], part=1) == 21919
+    assert main(raw=files["input"], part=2) == 9881048
+
+
+def solve():
+    print(f"\nSolving:")
+    answer1 = main(raw=files["input"], part=1)
+    print(f"Answer part1: {magenta_color}{answer1}{reset_color}")
+    answer2 = main(raw=files["input"], part=2)
+    print(f"Answer part2: {magenta_color}{answer2}{reset_color}")
+
+
+if __name__ == "__main__":
+    run_tests()
+    solve()
