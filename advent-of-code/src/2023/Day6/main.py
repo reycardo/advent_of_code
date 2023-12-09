2d21026e6169812cbb843be97fd61294fd00b166
reycardo <reycardo_95@hotmail.com>
2023-12-09 20:30:44 +0000

    🚨 running linter

--- advent-of-code/src/2023/Day6/main.py
+++ advent-of-code/src/2023/Day6/main.py
@@ -12,13 +12,22 @@ files = get_txt_files(__file__)
 class Puzzle:
     def __init__(self, text_input):
         self.input = text_input
-        self.input_parsed, self.input_parsed2 = self.parse_input()        
+        self.input_parsed, self.input_parsed2 = self.parse_input()
 
     def parse_input(self):
-        parsed_time = [int(x) for x in self.input[0].split(":")[1].strip().split(" ") if x]
-        parsed_distance = [int(x) for x in self.input[1].split(":")[1].strip().split(" ") if x]
+        parsed_time = [
+            int(x) for x in self.input[0].split(":")[1].strip().split(" ") if x
+        ]
+        parsed_distance = [
+            int(x) for x in self.input[1].split(":")[1].strip().split(" ") if x
+        ]
         parsed_input_1 = list(zip(parsed_time, parsed_distance))
-        parsed_input_2 = [(int(''.join(map(str, parsed_time))),int(''.join(map(str, parsed_distance))))]
+        parsed_input_2 = [
+            (
+                int("".join(map(str, parsed_time))),
+                int("".join(map(str, parsed_distance))),
+            )
+        ]
         return parsed_input_1, parsed_input_2
 
     def get_distance(self, time_pressed, max_time):
@@ -27,33 +36,38 @@ class Puzzle:
         return distance
 
     def run_all_distances(self, max_time):
-        return [self.get_distance(time_pressed, max_time) for time_pressed in range(max_time)]        
+        return [
+            self.get_distance(time_pressed, max_time)
+            for time_pressed in range(max_time)
+        ]
 
     def run_pt2_distances(self):
         record_passed = False
         time_pressed = 0
         while not record_passed:
-            if self.get_distance(time_pressed, self.input_parsed2[0][0]) > self.input_parsed2[0][1]:
-                record_passed = True                        
+            if (
+                self.get_distance(time_pressed, self.input_parsed2[0][0])
+                > self.input_parsed2[0][1]
+            ):
+                record_passed = True
                 return time_pressed
             time_pressed += 1
 
     def check_records(self, input):
-        ways_2_beat = []        
+        ways_2_beat = []
         for race in input:
             distances = self.run_all_distances(race[0])
             records = [distance for distance in distances if distance > race[1]]
             ways_2_beat.append(len(records))
         return ways_2_beat
 
-
     def solve(self, part):
         if part == 1:
             return reduce(lambda x, y: x * y, self.check_records(self.input_parsed))
         if part == 2:
             answer = self.input_parsed2[0][0] - 2 * self.run_pt2_distances() + 1
-            return answer 
-        #self.check_records(self.input_parsed2)[0]
+            return answer
+        # self.check_records(self.input_parsed2)[0]
 
 
 @timing_decorator
