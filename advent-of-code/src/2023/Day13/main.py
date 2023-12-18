2fe871e57cab5ae513b9d7d6dd9a19e953ef93a4
reycardo <reycardo_95@hotmail.com>
2023-12-18 13:38:38 +0000

    ⭐️Solved day13

--- advent-of-code/src/2023/Day13/main.py
+++ advent-of-code/src/2023/Day13/main.py
@@ -7,40 +7,49 @@ files = get_txt_files(__file__)
 # Start #
 #########
 
+
 class Pattern:
     def __init__(self, pattern) -> None:
         self.pattern = pattern
-        self.value = self.check_mirror()
+        self.value1 = self.check_mirror(difference_threshold=0)
+        self.value2 = self.check_mirror(difference_threshold=1)
 
-    def check_mirror(self):
-        for X in range(1,len(self.pattern)): # horizontal
+    def count_differences(self, l, r):
+        return sum(a != b for a, b in zip(l, r))
+
+    def check_mirror(self, difference_threshold=0):
+        for X in range(1, len(self.pattern)):  # horizontal
             Y = min(X, len(self.pattern) - X)
-            if self.pattern[X-Y:X] == self.pattern[X:X+Y][::-1]:
+            upper = self.pattern[X - Y : X]
+            lower = self.pattern[X : X + Y][::-1]
+            diff_count = sum(self.count_differences(a, b) for a, b in zip(upper, lower))
+            if diff_count == difference_threshold:
                 return 100 * X
-        for X in range(1,len(self.pattern[0])): # vertical
+
+        for X in range(1, len(self.pattern[0])):  # vertical
             Y = min(X, len(self.pattern[0]) - X)
-            if [pattern[X-Y:X] for pattern in self.pattern] == [pattern[X:X+Y][::-1] for pattern in self.pattern]:
+            left = [pattern[X - Y : X] for pattern in self.pattern]
+            right = [pattern[X : X + Y][::-1] for pattern in self.pattern]
+            diff_count = sum(self.count_differences(a, b) for a, b in zip(left, right))
+            if diff_count == difference_threshold:
                 return X
+
 
 class Puzzle:
     def __init__(self, text_input):
-        self.input = text_input 
+        self.input = text_input
         self.patterns = self.parse_input()
-        a=1       
+        a = 1
 
     def parse_input(self):
-        d = [
-            Pattern(list(sub[1]))
-            for sub in groupby(self.input, key=bool)
-            if sub[0]
-        ]
+        d = [Pattern(list(sub[1])) for sub in groupby(self.input, key=bool) if sub[0]]
         return d
 
     def solve(self, part):
         if part == 1:
-            return sum(pattern.value for pattern in self.patterns)
+            return sum(pattern.value1 for pattern in self.patterns)
         if part == 2:
-            pass
+            return sum(pattern.value2 for pattern in self.patterns)
 
 
 @timing_decorator
@@ -54,20 +63,20 @@ def main(raw, part):
 def run_tests():
     print(f"\nRunning Tests:")
     assert main(raw=files["test"], part=1) == 405
-    # assert main(raw=files["test"], part=2) == 71503
+    assert main(raw=files["test"], part=2) == 400
 
     # solutions
     print(f"\nRunning Solutions:")
     assert main(raw=files["input"], part=1) == 34202
-    # assert main(raw=files["input"], part=2) == 32607562
+    assert main(raw=files["input"], part=2) == 34230
 
 
 def solve():
     print(f"\nSolving:")
     answer1 = main(raw=files["input"], part=1)
     print(f"Answer part1: {magenta_color}{answer1}{reset_color}")
-    # answer2 = main(raw=files["input"], part=2)
-    # print(f"Answer part2: {magenta_color}{answer2}{reset_color}")
+    answer2 = main(raw=files["input"], part=2)
+    print(f"Answer part2: {magenta_color}{answer2}{reset_color}")
 
 
 if __name__ == "__main__":
