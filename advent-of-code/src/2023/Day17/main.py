d556d6d2b1980a86caf15a5ee5b0214f340e0f1a
reycardo <reycardo_95@hotmail.com>
2024-11-27 18:31:09 +0000

    reading input

--- advent-of-code/src/2023/Day17/main.py
+++ advent-of-code/src/2023/Day17/main.py
@@ -6,12 +6,21 @@ files = get_txt_files(__file__)
 # Start #
 #########
 
+class Block:
+    def __init__(self, heat_loss, pos) -> None:
+        self.heat_loss = heat_loss
+        self.pos = pos
 
 class Puzzle:
     def __init__(self, text_input):
         self.input = text_input
         self.input_parsed = [
-            [Tile(point, (x, y)) for x, point in enumerate(row)]
+            [
+                Block(
+                    heat_loss=heat_loss,
+                    pos=(x, y)
+                ) for x, heat_loss in enumerate(row)
+            ]
             for y, row in enumerate(self.input)
         ]
 
@@ -32,21 +41,21 @@ def main(raw, part):
 
 def run_tests():
     print(f"\nRunning Tests:")
-    assert main(raw=files["test"], part=1) == 46
-    assert main(raw=files["test"], part=2) == 51
+    assert main(raw=files["test"], part=1) == 102
+    # assert main(raw=files["test"], part=2) == 51
 
     # solutions
-    print(f"\nRunning Solutions:")
-    assert main(raw=files["input"], part=1) == 6978
-    assert main(raw=files["input"], part=2) == 7315  # 33s
+    # print(f"\nRunning Solutions:")
+    # assert main(raw=files["input"], part=1) == 6978
+    # assert main(raw=files["input"], part=2) == 7315
 
 
 def solve():
     print(f"\nSolving:")
     answer1 = main(raw=files["input"], part=1)
     print(f"Answer part1: {magenta_color}{answer1}{reset_color}")
-    answer2 = main(raw=files["input"], part=2)
-    print(f"Answer part2: {magenta_color}{answer2}{reset_color}")
+    # answer2 = main(raw=files["input"], part=2)
+    # print(f"Answer part2: {magenta_color}{answer2}{reset_color}")
 
 
 if __name__ == "__main__":
