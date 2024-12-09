243e898d8989a2d83bbd044434efd42e8db61dbe
reycardo <reycardo_95@hotmail.com>
2024-12-09 17:56:04 +0000

    day7 init

---
+++ advent-of-code/src/2024/Day7/main.py
@@ -0,0 +1,50 @@
+from utils.tools import get_txt_files, read_input, timing_decorator
+from utils.colors import magenta_color, reset_color
+
+files = get_txt_files(__file__)
+#########
+# Start #
+#########
+
+
+class Puzzle:
+    def __init__(self, text_input):
+        self.input = text_input
+
+    def solve(self, part):
+        if part == 1:
+            pass
+        elif part == 2:
+            pass
+
+
+@timing_decorator
+def main(raw, part):
+    text_input = read_input(raw)
+    input_parsed = [i if i else "" for i in text_input]
+    puzzle = Puzzle(input_parsed)
+    return puzzle.solve(part)
+
+
+def run_tests():
+    print(f"\nRunning Tests:")
+    assert main(raw=files["test"], part=1) == 3749
+    # assert main(raw=files["test"], part=2) == 123
+
+    # solutions
+    # print(f"\nRunning Solutions:")
+    # assert main(raw=files["input"], part=1) == 7365
+    # assert main(raw=files["input"], part=2) == 5770
+
+
+def solve():
+    print(f"\nSolving:")
+    answer1 = main(raw=files["input"], part=1)
+    print(f"Answer part1: {magenta_color}{answer1}{reset_color}")
+    # answer2 = main(raw=files["input"], part=2)
+    # print(f"Answer part2: {magenta_color}{answer2}{reset_color}")
+
+
+if __name__ == "__main__":
+    run_tests()
+    solve()
