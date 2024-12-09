d249c05bbe5cd90bae546dc442d292957acf32af
reycardo <reycardo_95@hotmail.com>
2024-12-09 17:46:11 +0000

    ⭐️ Solved day6 PT2

--- advent-of-code/src/2024/Day6/main.py
+++ advent-of-code/src/2024/Day6/main.py
@@ -9,139 +9,194 @@ files = get_txt_files(__file__)
 #########
 
 
-class Guard:
-    def __init__(self, pos: Point, front: str):
-        self.pos = pos
-        self.front = VectorDicts.REVERSE_ARROWS[front]
-        self.count = 1
-        self.count_obstructions = 0
+class GuardMap(Grid):
+    SPACE = "."
+    OBSTACLE = "#"
 
-    def turn(self):
-        available_dirs = list(VectorDicts.REVERSE_ARROWS.values())
-        current_index = available_dirs.index(self.front)
-        self.front = available_dirs[(current_index + 1) % len(available_dirs)]
+    DIRECTIONS = "^>v<"  # Each successive direction is the result of turning right (i.e. 90 degrees)
+    DIRECTIONS_MAP = {
+        "^": Point(0, -1),
+        ">": Point(1, 0),
+        "v": Point(0, 1),
+        "<": Point(-1, 0),
+    }
 
-    def detect_next_stop(self, grid: Grid):
-        current_point = self.pos
-        direction = self.front
-        previous_point = current_point
+    def __init__(self, grid_array: list):
+        super().__init__(grid_array)
+
+        self._all_obstacles = set()
+        self._update_all_obstacles()
+
+        self._guard_location = self._locate_guard()
+        self._start_location = self._guard_location
+
+        self._guard_direction = self.value_at_point(self._guard_location)
+        self._directions_idx = GuardMap.DIRECTIONS.index(self._guard_direction)
+        self._start_direction_idx = self._directions_idx
+
+        self._visited_with_direction = set()
+        self._visited_map = {}  # We can use this to print the route
+        self._visited: list[tuple[Point, str]] = []  # To track our route, if we need it
+        self._in_loop = False
+
+        self._update_visited()
+
+        self._pre_obstacle_added = None
+
+    def reset(self):
+        self._guard_location = self._start_location
+        self._guard_direction = self.value_at_point(self._guard_location)
+        self._directions_idx = self._start_direction_idx
+
+        self._visited_with_direction = set()
+        self._visited_map: dict[Point, str] = {}
+        self._visited = []
+        self._in_loop = False
+
+        self._update_visited()
+        self._clear_obstacle()
+
+    def add_obstacle(self, location: Point):
+        """Add an obstacle at the specified location.
+        Store this location so we can clear the obstacle later."""
+        self._pre_obstacle_added = (location, self.value_at_point(location))
+        self.set_value_at_point(location, GuardMap.OBSTACLE)
+
+    def _clear_obstacle(self):
+        """Clear any previously set obstacle."""
+        if self._pre_obstacle_added:
+            self.set_value_at_point(
+                self._pre_obstacle_added[0], self._pre_obstacle_added[1]
+            )
+
+    @property
+    def in_loop(self) -> bool:
+        """Are we stuck in a loop?"""
+        return self._in_loop
+
+    @property
+    def visited(self):
+        """Visited locations, as a dict of {location: direction, ...}"""
+        return self._visited_map
+
+    @property
+    def distinct_visited_count(self) -> int:
+        """Count of all distinct locations we've visited."""
+        return len(self._visited_map)
+
+    def _update_visited(self):
+        """Update visited locations"""
+
+        location_config = (self._guard_location, self._guard_direction)
+
+        # Update our dict of where we've been
+        self._visited_map[self._guard_location] = self._guard_direction
+        self._visited.append(location_config)
+
+        # For loop checking, we need to check if we've seen this location AND this orientation
+        if location_config in self._visited_with_direction:
+            self._in_loop = True  # We've done this before!
+        else:
+            self._visited_with_direction.add(location_config)
+
+    def _update_all_obstacles(self):
+        self._all_obstacles = set()
+
+        for point in self._all_points:
+            if self.value_at_point(point) == GuardMap.OBSTACLE:
+                self._all_obstacles.add(point)
+
+    def move(self) -> bool:
+        """
+        Move guard one space in current direction.
+        If we can't move forward in this direction, make turn and move.
+        Return True if we move, or False if we leave the map
+        """
         while True:
-            current_point += Point(*direction)
-            if not grid.valid_location(current_point):
-                return (previous_point, True)
-            elif grid.value_at_point(current_point)[0] == "#":
-                return (previous_point, False)
-            previous_point = current_point
+            # Move one step in the direction the guard is pointing
+            next_point = (
+                self._guard_location + GuardMap.DIRECTIONS_MAP[self._guard_direction]
+            )
 
-    def walk_to_object(self, grid: Grid, next_stop, part=1):
-        current_point = self.pos
-        direction = self.front
-        while current_point != next_stop:
-            current_point += Point(*direction)
-            if part == 2:
-                self.test_4_obstruction(grid=grid, current_point=current_point)
-            if not grid.value_at_point(current_point)[0] == "X":
-                grid.set_value_at_point(current_point, ("X", direction))
-                self.count += 1
-            elif (
-                grid.value_at_point(current_point)[0] == "X"
-                and grid.value_at_point(current_point)[1]
-            ):
-                old_dir = grid.value_at_point(current_point)[1]
-                directions = [old_dir]
-                directions.append(direction)
-                grid.set_value_at_point(current_point, ("X", directions))
-            self.pos = current_point
+            if not self.valid_location(next_point):  # leaving the map?
+                return False
 
-        self.turn()
-
-    def walk_till_loop(self, grid: Grid, next_stop):
-        current_point = self.pos
-        direction = self.front
-        while current_point != next_stop:
-            current_point += Point(*direction)
-            if not grid.value_at_point(current_point)[0] == "X":
-                grid.set_value_at_point(current_point, ("X", [direction]))
-            elif grid.value_at_point(current_point)[0] == "X":
-                if grid.value_at_point(current_point)[1]:
-                    if direction in grid.value_at_point(current_point)[1]:
-                        self.count += 1
-                        return True
-                    else:
-                        old_dir = grid.value_at_point(current_point)[1]
-                        directions = [old_dir]
-                        directions.append(direction)
-
-            self.pos = current_point
-
-        self.turn()
-        return False
-
-    def test_4_obstruction(self, grid: Grid, current_point):
-        new_grid = Grid([row[:] for row in grid._array])  # Deep copy of the grid
-        new_grid = self.add_obstacle(current_point, new_grid)
-        previous_dir = self.front
-        self.turn()
-        loop = False
-        is_done = False
-        while not is_done:
-            next_stop, is_done = self.detect_next_stop(grid=new_grid)
-            if not is_done:
-                loop = self.walk_till_loop(grid=new_grid, next_stop=next_stop)
-                if loop:
-                    self.count_obstructions += 1
-                    break
-        self.front = previous_dir
-
-    def add_obstacle(self, location: Point, grid: Grid):
-        self.new_obstacle_location = (location, grid.value_at_point(location))
-        grid.set_value_at_point(location, "#")
-        return grid
-
-
-class Puzzle:
-    def __init__(self, text_input):
-        self.input = text_input
-        self.grid = Grid([list(line) for line in self.input])
-        self.find_guard()
-
-    def find_guard(self):
-        guard_point = next(
-            (
-                x
-                for x in self.grid.all_points()
-                if self.grid.value_at_point(x) in VectorDicts.ARROWS
-            ),
-            None,
-        )
-        if guard_point is not None:
-            self.guard = Guard(guard_point, self.grid.value_at_point(guard_point))
-
-    def solve(self, part):
-        if part == 1:
-            is_done = False
-            self.grid.set_value_at_point(self.guard.pos, ("X", self.guard.front))
-            while not is_done:
-                next_stop, is_done = self.guard.detect_next_stop(grid=self.grid)
-                self.guard.walk_to_object(grid=self.grid, next_stop=next_stop)
-            return self.guard.count
-        elif part == 2:
-            is_done = False
-            self.grid.set_value_at_point(self.guard.pos, ("X", self.guard.front))
-            while not is_done:
-                next_stop, is_done = self.guard.detect_next_stop(grid=self.grid)
-                self.guard.walk_to_object(
-                    grid=self.grid, next_stop=next_stop, part=part
+            # Are we at an obstacle? If so, rotate right and try again
+            next_value = self.value_at_point(next_point)
+            if next_value == GuardMap.OBSTACLE:
+                # Increment the direction index
+                self._directions_idx = (self._directions_idx + 1) % len(
+                    GuardMap.DIRECTIONS
                 )
-            return self.guard.count_obstructions
+                self._guard_direction = GuardMap.DIRECTIONS[self._directions_idx]
+                continue
+            else:  # No obstacle, so we can move to this location
+                self._guard_location = next_point
+                self._update_visited()
+                break  # We've successfully moved
+
+        return True
+
+    def _locate_guard(self) -> Point:
+        for point in self.all_points():
+            if self.value_at_point(point) in GuardMap.DIRECTIONS:
+                return point
+
+    def __str__(self) -> str:
+        row_strs = []
+        for y, row in enumerate(self._array):
+            row_list = []
+            for x, char in enumerate(row):
+                locn = Point(x, y)
+                if locn in self._visited_map.keys():
+                    row_list.extend([self._visited_map[locn]])
+                else:
+                    row_list.append(char)
+
+            row_strs.append("".join(row_list))
+
+        return "\n".join(row_strs)
+
+
+def solve_p1(data):
+    guard_map = GuardMap(data)
+    while guard_map.move():
+        pass
+    return guard_map.distinct_visited_count
+
+
+def solve_pt2(data):
+    # Initial route
+    guard_map = GuardMap(data)
+    while guard_map.move():
+        pass
+
+    # Route taken, excluding starting point
+    route = [locn for locn in guard_map.visited.keys()][1:]
+
+    loop_locations = 0
+
+    for location in route:
+        guard_map.reset()
+        guard_map.add_obstacle(location)
+        while guard_map.move():
+            if guard_map.in_loop:
+                loop_locations += 1
+                break
+
+    print(f"Found {loop_locations} loop locations.")
+    return loop_locations
 
 
 @timing_decorator
 def main(raw, part):
     text_input = read_input(raw)
     input_parsed = [i if i else "" for i in text_input]
-    puzzle = Puzzle(input_parsed)
-    return puzzle.solve(part)
+    input_parsed = [list(line) for line in input_parsed]
+    if part == 1:
+        return solve_p1(input_parsed)
+    if part == 2:
+        return solve_pt2(input_parsed)
 
 
 def run_tests():
@@ -152,7 +207,7 @@ def run_tests():
     # solutions
     print(f"\nRunning Solutions:")
     assert main(raw=files["input"], part=1) == 4883
-    # assert main(raw=files["input"], part=2) == 5770
+    assert main(raw=files["input"], part=2) == 1655
 
 
 def solve():
--- advent-of-code/utils/tools.py
+++ advent-of-code/utils/tools.py
@@ -8,6 +8,7 @@ from itertools import product
 from enum import Enum
 from dataclasses import asdict, dataclass
 
+
 def read_input(file, sep: str = "\n"):
     with open(file, "r") as tf:
         return tf.read().strip().split(sep)
@@ -130,9 +131,11 @@ def format_elapsed_time(seconds: float) -> str:
     else:
         return f"{ms}ms"
 
+
 @dataclass(frozen=True)
 class Point:
-    """ Class for storing a point x,y coordinate """
+    """Class for storing a point x,y coordinate"""
+
     x: int
     y: int
 
@@ -140,7 +143,7 @@ class Point:
         return Point(self.x + other.x, self.y + other.y)
 
     def __mul__(self, other: Point):
-        """ (x, y) * (a, b) = (xa, yb) """
+        """(x, y) * (a, b) = (xa, yb)"""
         return Point(self.x * other.x, self.y * other.y)
 
     def __sub__(self, other: Point):
@@ -149,13 +152,17 @@ class Point:
     def __lt__(self, other):
         # Arbitrary comparison logic
         return (self.x, self.y) < (other.x, other.y)
-    
+
     def yield_neighbours(self, include_diagonals=True, include_self=False):
-        """ Generator to yield neighbouring Points """
+        """Generator to yield neighbouring Points"""
 
         deltas: list
         if not include_diagonals:
-            deltas = [vector.value for vector in Vectors if abs(vector.value[0]) != abs(vector.value[1])]
+            deltas = [
+                vector.value
+                for vector in Vectors
+                if abs(vector.value[0]) != abs(vector.value[1])
+            ]
         else:
             deltas = [vector.value for vector in Vectors]
 
@@ -166,35 +173,41 @@ class Point:
             yield Point(self.x + delta[0], self.y + delta[1])
 
     def neighbours(self, include_diagonals=True, include_self=False) -> list[Point]:
-        """ Return all the neighbours, with specified constraints.
-        It wraps the generator with a list. """
+        """Return all the neighbours, with specified constraints.
+        It wraps the generator with a list."""
         return list(self.yield_neighbours(include_diagonals, include_self))
 
     def get_specific_neighbours(self, directions: list[Vectors]) -> list[Point]:
-        """ Get neighbours, given a specific list of allowed locations """
+        """Get neighbours, given a specific list of allowed locations"""
         return [(self + Point(*vector.value)) for vector in list(directions)]
-    
-    def get_scaled_neighbours(self, directions: list[Vectors], scalar: int) -> list[Point]:
-        """ Get neighbours, given a specific list of allowed locations, scaled by a scalar """
-        return [(self + Point(vector.value[0] * scalar, vector.value[1] * scalar)) for vector in directions]
 
+    def get_scaled_neighbours(
+        self, directions: list[Vectors], scalar: int
+    ) -> list[Point]:
+        """Get neighbours, given a specific list of allowed locations, scaled by a scalar"""
+        return [
+            (self + Point(vector.value[0] * scalar, vector.value[1] * scalar))
+            for vector in directions
+        ]
 
     @staticmethod
     def manhattan_distance(a_point: Point) -> int:
-        """ Return the Manhattan distance value of this vector """
+        """Return the Manhattan distance value of this vector"""
         return sum(abs(coord) for coord in asdict(a_point).values())
 
     def manhattan_distance_from(self, other: Point) -> int:
-        """ Manhattan distance between this Vector and another Vector """
-        diff = self-other
+        """Manhattan distance between this Vector and another Vector"""
+        diff = self - other
         return Point.manhattan_distance(diff)
 
     def __repr__(self):
         return f"P({self.x},{self.y})"
 
+
 class Vectors(Enum):
-    """ Enumeration of 8 directions.
-    Note: y axis increments in the North direction, i.e. N = (0, 1) """
+    """Enumeration of 8 directions.
+    Note: y axis increments in the North direction, i.e. N = (0, 1)"""
+
     N = (0, 1)
     NE = (1, 1)
     E = (1, 0)
@@ -206,87 +219,92 @@ class Vectors(Enum):
 
     @property
     def y_inverted(self):
-        """ Return vector, but with y-axis inverted. I.e. N = (0, -1) """
+        """Return vector, but with y-axis inverted. I.e. N = (0, -1)"""
         x, y = self.value
         return (x, -y)
 
-class VectorDicts():
-    """ Contains constants for Vectors """
+
+class VectorDicts:
+    """Contains constants for Vectors"""
+
     ARROWS = {
-        '^': Vectors.N.value,
-        '>': Vectors.E.value,
-        'v': Vectors.S.value,
-        '<': Vectors.W.value
+        "^": Vectors.N.value,
+        ">": Vectors.E.value,
+        "v": Vectors.S.value,
+        "<": Vectors.W.value,
     }
 
     REVERSE_ARROWS = {
-        '^': Vectors.S.value,
-        '>': Vectors.E.value,
-        'v': Vectors.N.value,
-        '<': Vectors.W.value
+        "^": Vectors.S.value,
+        ">": Vectors.E.value,
+        "v": Vectors.N.value,
+        "<": Vectors.W.value,
     }
 
     DIRS = {
-        'U': Vectors.N.value,
-        'R': Vectors.E.value,
-        'D': Vectors.S.value,
-        'L': Vectors.W.value
+        "U": Vectors.N.value,
+        "R": Vectors.E.value,
+        "D": Vectors.S.value,
+        "L": Vectors.W.value,
     }
 
     NINE_BOX: dict[str, tuple[int, int]] = {
         # x, y vector for adjacent locations
-        'tr': (1, 1),
-        'mr': (1, 0),
-        'br': (1, -1),
-        'bm': (0, -1),
-        'bl': (-1, -1),
-        'ml': (-1, 0),
-        'tl': (-1, 1),
-        'tm': (0, 1)
+        "tr": (1, 1),
+        "mr": (1, 0),
+        "br": (1, -1),
+        "bm": (0, -1),
+        "bl": (-1, -1),
+        "ml": (-1, 0),
+        "tl": (-1, 1),
+        "tm": (0, 1),
     }
 
-class Grid():
-    """ 2D grid of point values. """
+
+class Grid:
+    """2D grid of point values."""
+
     def __init__(self, grid_array: list) -> None:
         self._array = grid_array
         self._width = len(self._array[0])
         self._height = len(self._array)
+        self._all_points = [
+            Point(x, y) for y in range(self._height) for x in range(self._width)
+        ]
 
     def value_at_point(self, point: Point) -> int:
-        """ The value at this point """
+        """The value at this point"""
         return self._array[point.y][point.x]
 
-    def set_value_at_point(self, point: Point, value: int):
+    def set_value_at_point(self, point: Point, value):
         self._array[point.y][point.x] = value
 
-
     def valid_location(self, point: Point) -> bool:
-        """ Check if a location is within the grid """
-        if (0 <= point.x < self._width and  0 <= point.y < self._height):
+        """Check if a location is within the grid"""
+        if 0 <= point.x < self._width and 0 <= point.y < self._height:
             return True
 
         return False
 
     @property
     def width(self):
-        """ Array width (cols) """
+        """Array width (cols)"""
         return self._width
 
     @property
     def height(self):
-        """ Array height (rows) """
+        """Array height (rows)"""
         return self._height
 
     def all_points(self) -> list[Point]:
-        points = [Point(x, y) for x in range(self.width) for y in range(self.height)]
-        return points
+        return self._all_points
 
     def rows_as_str(self):
-        """ Return the grid """
+        """Return the grid"""
         return ["".join(str(char) for char in row) for row in self._array]
 
     def cols_as_str(self):
-        """ Render columns as str. Returns: list of str """
+        """Render columns as str. Returns: list of str"""
         cols_list = list(zip(*self._array))
         return ["".join(str(char) for char in col) for col in cols_list]
 
@@ -294,4 +312,4 @@ class Grid():
         return f"Grid(size={self.width}*{self.height})"
 
     def __str__(self) -> str:
-        return "\n".join("".join(map(str, row)) for row in self._array)    
+        return "\n".join("".join(map(str, row)) for row in self._array)
