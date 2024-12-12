d2554dbd8fdb9b1bce0dd39478384f210f140a32
reycardo <reycardo_95@hotmail.com>
2024-12-12 18:11:54 +0000

    ⭐️ Solved day12 pt1

--- advent-of-code/src/2024/Day12/main.py
+++ advent-of-code/src/2024/Day12/main.py
@@ -1,121 +1,103 @@
 from __future__ import annotations
 from utils.tools import get_txt_files, read_input, timing_decorator
 from utils.colors import magenta_color, reset_color
-from utils.tools import Grid, Point, VectorDicts
-from typing import List, Dict
+from utils.tools import Grid, Point
+from typing import List
+from collections import deque
+from dataclasses import dataclass
 
 files = get_txt_files(__file__)
 #########
 # Start #
 #########
 
-class GardenRegion:
-    def __init__(self, initial_plot: GardenPlot, grid: Grid):         
-        self.plots: List[GardenPlot] = []
-        self.area = 0
-        self.perimiter = 0
-        self.price = 0 
-        self.neighbours = set()
-        initial_neihbours = initial_plot.position.neighbours(include_diagonals=False, include_self=False)
-        initial_neihbours = [x for x in initial_neihbours  if grid.valid_location(x)]
-        self.add_neighbours(initial_plot, initial_neihbours)
-        self.add_plot(initial_plot, grid)
-        self.region_type = self.plots[0].plant_type
 
-    def set_area(self):
-        self.area = len(self.plots)
+@dataclass(frozen=True)
+class Region:
+    plot_type: str
+    plots: set[Point]
+    perimeter: int
 
-    def set_perimiter(self):
-        for plot in self.plots:
-            neighbours = plot.position.neighbours(include_diagonals=False, include_self=False)
-            for neighbour in neighbours:
-                if neighbour not in [x.position for x in self.plots]:
-                    self.perimiter += 1
+    @property
+    def area(self) -> int:
+        return len(self.plots)
 
-    def set_price(self):
-        self.price = self.area * self.perimiter
+    @property
+    def price(self) -> int:
+        return self.area * self.perimeter
 
-    def add_neighbours(self, plot: GardenPlot, neighbours: List[Point]):        
-        if not self.neighbours:
-            self.neighbours = set(neighbours)
-        else:
-            # append to self.neighbours only the points that are not already in self.neighbours
-            self.neighbours = self.neighbours.union(set(neighbours))
-        
-        # if plot in self.neighbours remove from self.neighbours
-        if plot.position in self.neighbours:
-            self.neighbours.remove(plot.position)        
-        # remove neighbours that are already in self.plots
-        self.neighbours = self.neighbours.difference(set([plot.position for plot in self.plots]))
-            
-                        
-    def add_plot(self, plot: GardenPlot, grid: Grid):
-        plot_neighbours = plot.position.neighbours(include_diagonals=False, include_self=False)
-        plot_neighbours = [x for x in plot_neighbours  if grid.valid_location(x)]
-        if any(plot_neighbour in [plot.position for plot in self.plots] for plot_neighbour in plot_neighbours):
-            self.plots.append(plot)            
-            self.add_neighbours(plot, plot_neighbours)
-        elif not self.plots:
-            self.plots.append(plot)
-            self.add_neighbours(plot, plot_neighbours)
+    def __str__(self):
+        return f"Region(type={self.plot_type},area={self.area},perimeter={self.perimeter},price={self.price})"
 
-class GardenPlot:
-    def __init__(self, plant_type: str, position: Point):
-        self.plant_type = plant_type
-        self.position = position
 
-class Puzzle:
-    def __init__(self, text_input):
-        self.input = text_input
-        self.input_parsed = [list(x) for x in self.input]        
-        self.grid = Grid(self.input_parsed)
-        self.plots = [GardenPlot(self.grid.value_at_point(x),x) for x in self.grid._all_points]
-        self.regions = self.add_to_region()
-        self.aggregate_neighbouring_regions_of_same_region_type()
+class Garden(Grid):
+    def __init__(self, grid_array):
+        super().__init__(grid_array)
 
-    def add_to_region(self):
-        regions: List[GardenRegion] = []
-        for plot in self.plots:
-            # find a GardenRegion in regions that has the same region_type as plot.plant_type and is neighbour to plot
-            # if a region was found add the plot to the region
-            # else create a new region and add the plot to it
-            region = next((x for x in regions if x.region_type == plot.plant_type and plot.position in x.neighbours), None)
-            if region:
-                region.add_plot(plot, self.grid)
-            else:
-                new_region = GardenRegion(plot, self.grid)
-                regions.append(new_region)
+        self._regions = self._find_regions()
+
+    @property
+    def regions(self):
+        return self._regions
+
+    def _find_regions(self):
+        regions: list[Region] = []
+        allocated = set()
+
+        for point in self._all_points:
+            if point in allocated:
+                continue  # We've done this one
+
+            region_type = self.value_at_point(point)
+            region_plots, perimeter = self._flood_fill_for_origin(point)
+            region = Region(region_type, region_plots, perimeter)
+            regions.append(region)
+
+            allocated.update(region_plots)
+
         return regions
-    
-    def aggregate_neighbouring_regions_of_same_region_type(self):
-        for region in self.regions:
-            region_neighbours = [x for x in self.regions if x.region_type == region.region_type and x != region]
-            for neighbour in region_neighbours:
-                if any(plot.position in region.neighbours for plot in neighbour.plots):
-                    region.plots.extend(neighbour.plots)
-                    region.neighbours = region.neighbours.union(neighbour.neighbours)
-                    region.neighbours = region.neighbours.difference(set([plot.position for plot in self.plots]))
-                    self.regions.remove(neighbour)
-                    break   
 
+    def _flood_fill_for_origin(self, origin: Point):
+        region_type = self.value_at_point(origin)
 
-    def calc_areas(self):
-        for region in self.regions:
-            region.set_area()
+        frontier: deque[Point] = deque()  # Very efficient for FIFO queueing
+        frontier.append(origin)
 
-    def calc_perimiters(self):
-        for region in self.regions:
-            region.set_perimiter()
+        seen = set()
+        seen.add(origin)
 
-    def calc_prices(self):
-        for region in self.regions:
-            region.set_price()
+        plots = set()
+        perimeter = 0
+
+        while frontier:
+            current: Point = frontier.popleft()  # BFS
+            current_val = self.value_at_point(current)
+
+            if current_val == region_type:
+                plots.add(current)
+
+            # Get valid neighbours
+            neighbours = [
+                neighbour
+                for neighbour in current.neighbours(include_diagonals=False)
+                if (
+                    self.valid_location(neighbour)
+                    and self.value_at_point(neighbour) == region_type
+                )
+            ]
+
+            # For every neighbour that isn't valid, we have established a perimeter unit
+            perimeter += 4 - len(neighbours)
+
+            for next in neighbours:
+                if next not in seen:
+                    frontier.append(next)
+                    seen.add(next)
+
+        return plots, perimeter
 
     def solve(self, part):
         if part == 1:
-            self.calc_areas()
-            self.calc_perimiters()
-            self.calc_prices()
             return sum([x.price for x in self.regions])
         elif part == 2:
             pass
@@ -125,7 +107,7 @@ class Puzzle:
 def main(raw, part):
     text_input = read_input(raw)
     input_parsed = [i if i else "" for i in text_input]
-    puzzle = Puzzle(input_parsed)
+    puzzle = Garden(input_parsed)
     return puzzle.solve(part)
 
 
@@ -135,12 +117,19 @@ def run_tests():
     assert main(raw=files["test2"], part=1) == 772
     assert main(raw=files["test3"], part=1) == 1930
     assert main(raw=files["test4"], part=1) == 280
-    # assert main(raw=files["test"], part=2) == 81
-    
+    assert main(raw=files["test5"], part=1) == 426452
+    assert main(raw=files["test6"], part=1) == 1202
+    assert main(raw=files["test7"], part=1) == 2566
+
+    assert main(raw=files["test"], part=2) == 80
+    assert main(raw=files["test2"], part=2) == 436
+    assert main(raw=files["p2_test"], part=2) == 236
+    assert main(raw=files["p2_test2"], part=2) == 368
+    assert main(raw=files["test3"], part=2) == 1206
 
     # solutions
-    # print(f"\nRunning Solutions:")
-    # assert main(raw=files["input"], part=1) == 717
+    print(f"\nRunning Solutions:")
+    assert main(raw=files["input"], part=1) == 1464678
     # assert main(raw=files["input"], part=2) == 1686
 
 
@@ -148,8 +137,8 @@ def solve():
     print(f"\nSolving:")
     answer1 = main(raw=files["input"], part=1)
     print(f"Answer part1: {magenta_color}{answer1}{reset_color}")
-    # answer2 = main(raw=files["input"], part=2)
-    # print(f"Answer part2: {magenta_color}{answer2}{reset_color}")
+    answer2 = main(raw=files["input"], part=2)
+    print(f"Answer part2: {magenta_color}{answer2}{reset_color}")
 
 
 if __name__ == "__main__":
