fd21a38c5db832cb8064ded04f263036860e4f7a
reycardo <reycardo_95@hotmail.com>
2024-12-21 01:47:25 +0000

    ⭐️ Solved day20 pt2

--- advent-of-code/src/2024/Day20/main.py
+++ advent-of-code/src/2024/Day20/main.py
@@ -97,19 +97,19 @@ class Puzzle:
 
         return explored, trails
 
-    def find_cheats(self, point: Point, score_at_point: int):        
+    def find_cheats(self, point: Point, score_at_point: int):
         for direction in Puzzle.DIRECTIONS:
             next_point_1 = point + Point(direction.value)
             next_point_2 = next_point_1 + Point(direction.value)
             if (
-                self.grid.valid_location(next_point_1) and 
-                self.grid.valid_location(next_point_2) and 
-                self.grid.value_at_point(next_point_1) == Puzzle.WALL and 
+                self.grid.valid_location(next_point_1) and
+                self.grid.valid_location(next_point_2) and
+                self.grid.value_at_point(next_point_1) == Puzzle.WALL and
                 (self.grid.value_at_point(next_point_2) == Puzzle.SPACE or self.grid.value_at_point(next_point_2) == Puzzle.END)
             ):
                 if next_point_1 in self.cheats:
                     continue
-                self.cheats.add(next_point_1)                
+                self.cheats.add(next_point_1)
                 score_at_point
                 original_score = self.original_trail[0][0].index(next_point_2)
                 new_score = score_at_point + 1 + (len(self.original_trail[0][0] ) - 1 - original_score)
@@ -122,12 +122,31 @@ class Puzzle:
                         count, trails = self.saved_time[time_diff]
                         self.saved_time[time_diff] = (count + 1, trails + [new_trail])
                 self.grid.set_value_at_point(next_point_1, Puzzle.WALL)
+    
+    @timing_decorator
+    def find_new_cheats(self, point: Point, score_at_point: int):
+        for i in range(score_at_point, len(self.original_trail[0][0])): # might need to change len
+            next_point_2: Point = self.original_trail[0][0][i]
+            man_dist = next_point_2.manhattan_distance_from(point)
+            if man_dist <= 20:            
+                # if so check if time_diff > 50 
+                original_score = self.original_trail[0][0].index(next_point_2)
+                new_score = score_at_point + man_dist + (len(self.original_trail[0][0] ) - 2 - original_score)
+                time_diff = self.trail_time - new_score
+                if time_diff not in self.saved_time:
+                    self.saved_time[time_diff] = 1
+                else:
+                    self.saved_time[time_diff] += 1                
 
 
-
-    def cheat(self, trail: List[Point]):
-        for score, point in enumerate(trail, start=1):
-            self.find_cheats(point, score_at_point=score)
+    def cheat(self, trail: List[Point], part):
+        if part==1:
+            for score, point in enumerate(trail, start=1):
+                self.find_cheats(point, score_at_point=score)
+        elif part==2:
+            for score, point in enumerate(trail, start=1):
+                print(f"finding new cheats for {point}")
+                self.find_new_cheats(point, score_at_point=score)
 
     def paint_grid(self, grid: Grid, points_to_paint: List[Point]):
         for point in points_to_paint:
@@ -137,13 +156,14 @@ class Puzzle:
             grid.set_value_at_point(point, ".")
 
     def solve(self, part):
-        if part == 1:
-            _, self.original_trail = self.find_path(self.grid)
-            self.trail_time = min(self.original_trail, key=lambda x: x[1])[1]
-            self.cheat(self.original_trail[0][0])            
+        _, self.original_trail = self.find_path(self.grid)
+        self.trail_time = min(self.original_trail, key=lambda x: x[1])[1]        
+        self.cheat(self.original_trail[0][0], part=part)
+        if part==1:
             return sum([count[0] for saved_time, count in self.saved_time.items() if saved_time >= self.save])
-        elif part == 2:
-            pass
+        elif part==2:
+            return sum([count for saved_time, count in self.saved_time.items() if saved_time >= self.save])
+
 
 @timing_decorator
 def main(raw, part, save=100):
@@ -156,7 +176,7 @@ def main(raw, part, save=100):
 def run_tests():
     print(f"\nRunning Tests:")
     assert main(raw=files["test"], part=1, save=10) == 10
-    # assert main(raw=files["test"], part=2) == 16
+    assert main(raw=files["test"], part=2, save=50) == 32+31+29+39+25+23+20+19+12+14+12+22+4+3
 
     # solutions
     # print(f"\nRunning Solutions:")
@@ -168,8 +188,8 @@ def solve():
     print(f"\nSolving:")
     answer1 = main(raw=files["input"], part=1, save=100)
     print(f"Answer part1: {magenta_color}{answer1}{reset_color}")
-    # answer2 = main(raw=files["input"], part=2)
-    # print(f"Answer part2: {magenta_color}{answer2}{reset_color}")
+    answer2 = main(raw=files["input"], part=2, save=100)
+    print(f"Answer part2: {magenta_color}{answer2}{reset_color}")
 
 
 if __name__ == "__main__":
