6d1bec0a4902f9b20a7e1bf968e96ba7a0603d00
reycardo <reycardo_95@hotmail.com>
2024-12-23 16:06:06 +0000

    ⭐️ Solved day23

--- advent-of-code/src/2024/Day23/main.py
+++ advent-of-code/src/2024/Day23/main.py
@@ -20,27 +20,37 @@ class Puzzle:
         self.setup_graph()
 
     def parse_input(self):
-        return [path.split('-') for path in self.input]    
+        return [path.split("-") for path in self.input]
 
-    def setup_graph(self):    
-        self.G = nx.Graph()    
+    def setup_graph(self):
+        self.G = nx.Graph()
         self.G.add_edges_from(self.input_parsed)
 
     def find_sets_of_count(self, count: int):
         sets = set()
         for node, adjacents in self.G._adj.items():
-            if node.startswith('t'):
-                for combo in combinations(adjacents, count-1):
-                    if all(combo[i] in self.G._adj[combo[j]] for i in range(len(combo)) for j in range(i + 1, len(combo))):
+            if node.startswith("t"):
+                for combo in combinations(adjacents, count - 1):
+                    if all(
+                        combo[i] in self.G._adj[combo[j]]
+                        for i in range(len(combo))
+                        for j in range(i + 1, len(combo))
+                    ):
                         sets.add(frozenset((node, *combo)))
         return sets
+
+    def find_cliques(self):
+        biggest_clique = max(nx.find_cliques(self.G), key=len)
+        return biggest_clique
 
     def solve(self, part):
         if part == 1:
             sets_of_3 = self.find_sets_of_count(3)
             return len(sets_of_3)
         elif part == 2:
-            pass
+            biggest_clique = self.find_cliques()
+            ordered_biggest_clique = ",".join((sorted(biggest_clique)))
+            return ordered_biggest_clique
 
 
 @timing_decorator
@@ -54,11 +64,11 @@ def main(raw, part):
 def run_tests():
     print(f"\nRunning Tests:")
     assert main(raw=files["test"], part=1) == 7
-    # assert main(raw=files["test2"], part=2) == 23
+    assert main(raw=files["test"], part=2) == "co,de,ka,ta"
 
     # solutions
-    # print(f"\nRunning Solutions:")
-    # assert main(raw=files["input"], part=1) == 16619522798
+    print(f"\nRunning Solutions:")
+    assert main(raw=files["input"], part=1) == 1476
     # assert main(raw=files["input"], part=2) == 662726441391898
 
 
@@ -66,8 +76,8 @@ def solve():
     print(f"\nSolving:")
     answer1 = main(raw=files["input"], part=1)
     print(f"Answer part1: {magenta_color}{answer1}{reset_color}")
-    # answer2 = main(raw=files["input"], part=2)
-    # print(f"Answer part2: {magenta_color}{answer2}{reset_color}")
+    answer2 = main(raw=files["input"], part=2)
+    print(f"Answer part2: {magenta_color}{answer2}{reset_color}")
 
 
 if __name__ == "__main__":
